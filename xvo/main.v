6bedc508660eb4d33b8dcc400ca2dba505f66b97
scuderia666 <satouuxd@gmail.com>
2022-08-15 21:25:25 +0300

    up

--- xvo/main.v
+++ xvo/main.v
@@ -1,10 +1,50 @@
 import os
 import vargs
 import lol
+import pkg { Package }
 
 const (
 	version = '0.1'
 )
+
+struct Packages {
+	packages map[string]int
+}
+
+fn usage() {
+	println("xvo build|install <pkg>")
+	exit(1)
+}
+
+fn do_build() {
+
+}
+
+fn do_install() {
+
+}
+
+fn do_action(action string, pkg string) {
+	println('$action: $pkg')
+
+	match action {
+		'build' {
+			do_build()
+		}
+
+		'install' {
+			do_install()
+		}
+
+		else { }
+	}
+}
+
+fn read_package(name string) {
+	pkg := Package{name}
+
+	packages[name] = pkg
+}
 
 fn main() {
 	mut args := vargs.new(os.args, 1)
@@ -13,12 +53,29 @@ fn main() {
 
 	args.parse()
 
-	if args.command.len > 0 {
-		println(args.command)
+	if args.command.len == 0 {
+		exit(1)
+	}
+
+	mut action := ''
+
+	match args.command {
+		'build' {
+			action = 'build'
+		}
+
+		'install' {
+			action = 'install'
+		}
+
+		else { usage() }
 	}
 
 	if args.unknown.len > 0 {
-		println(args.unknown[0])
+		for _, pkg in args.unknown {
+			read_package(pkg)
+			do_action(action, pkg)
+		}
 	}
 
 	if args.options.len > 0 {
